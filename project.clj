d5a510da3ec36d5af41459d917e565ca03ad55d7
Kenny Evitt <kevitt@recp.com>
2015-01-14 15:55:20 -0500

    Change project version and add private repositories.
    Case 1405

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
-(defproject com.draines/postal "1.12-SNAPSHOT"
+(defproject com.draines/postal "1.12-recp-1-SNAPSHOT"
   :description "JavaMail on Clojure"
   :url "https://github.com/drewr/postal"
   :license {:name "MIT"
             :url "http://en.wikipedia.org/wiki/MIT_License"
             :distribution :repo
             :comments "Use at your own risk"}
-  :repositories {"java.net" "http://download.java.net/maven/2"}
+  :repositories [["java.net" "http://download.java.net/maven/2"]
+                 ["snapshots" :url "http://snydevel:8080/repository/snapshots/"]
+                 ["releases" :url "http://snydevel:8080/repository/internal/"]]
   :dependencies [[commons-codec "1.9"]
                  [javax.mail/mail "1.4.4"
                   :exclusions [javax.activation/activation]]]
